98bf8f90abe7680d3fca46d85d1afa1590b4a210
thompson <so.wo@foxmail.com>
2022-08-03 17:19:43 +0800

    Revert "增加bulkWrite实际插入数据的数量 (#51)"
    This reverts commit afaecbac4354fdd83773d625c64b16254a8df14d.

--- src/MongoClient/Type/BulkWriteResult.php
+++ src/MongoClient/Type/BulkWriteResult.php
@@ -37,11 +37,6 @@ class BulkWriteResult implements Unserializable
     private $deletedCount;
 
     /**
-     * @var int
-     */
-    private $insertedCount;
-
-    /**
      * @var array<ObjectId>
      */
     private $upsertedIds;
@@ -52,7 +47,6 @@ class BulkWriteResult implements Unserializable
         $this->modifiedCount = $data['modifiedcount'];
         $this->upsertedCount = $data['upsertedcount'];
         $this->deletedCount = $data['deletedcount'];
-        $this->insertedCount = $data['insertedcount'];
         $this->upsertedIds = (array) $data['upsertedids'];
     }
 
@@ -80,10 +74,4 @@ class BulkWriteResult implements Unserializable
     {
         return (array) $this->upsertedIds;
     }
-
-    public function getinsertedCount(): int
-    {
-        return $this->insertedCount;
-    }
-
 }
